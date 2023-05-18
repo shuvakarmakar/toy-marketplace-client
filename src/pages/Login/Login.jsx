503a322cd08235a6334a99731d002b33e7334283
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-18 15:01:32 +0600

    Login Page Frontend work done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,49 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password);
+    }
+
     return (
-        <div>
-            
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="mr-10 w-1/2">
+                    <img src="" alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-3xl font-bold">Login now!</h1>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" value="Login" className="btn btn-primary" />
+                            </div>
+                        </form>
+                        <p className='my-4 text-center'>New to Toy World ? Please <Link className='text-amber-300' to='/signup'>Sign Up</Link></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

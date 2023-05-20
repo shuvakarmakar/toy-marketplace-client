243d530b59a06cd39872fb9ba353059ef47855e3
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-21 02:38:57 +0600

    My Toy Page Update Done

---
+++ src/pages/Home/MyToys/MyToy.jsx
@@ -0,0 +1,39 @@
+
+const MyToy = ({ myToy }) => {
+    const { name, sellerName, subCategory, price, availableQuantity, _id, photoUrl } = myToy || {};
+    return (
+        <tr>
+            <th>
+                <label>
+                    <input type="checkbox" className="checkbox" />
+                </label>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle w-20 h-20">
+                            {photoUrl && <img src={photoUrl} alt="Avatar Tailwind CSS Component" />}
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{sellerName}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {name}
+            </td>
+            <td>{subCategory}</td>
+            <td>{price}</td>
+            <td>{availableQuantity}</td>
+            <th>
+                <button className="btn btn-info">Update</button>
+                <button className="btn btn-sm btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </th>
+        </tr>
+    );
+};
+
+export default MyToy;
--- src/pages/Home/MyToys/MyToys.jsx
+++ src/pages/Home/MyToys/MyToys.jsx
@@ -1,10 +1,53 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
+import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
+import MyToy from "./MyToy";
 
 const MyToys = () => {
+
+    const { user } = useContext(AuthContext);
+    const [myToys, setMyToys] = useState([])
+
+    useEffect(() => {
+        fetch(`https://toy-marketplace-server-red.vercel.app/myToys/${user?.email}`)
+            .then(result => result.json())
+            .then((data) => {
+                setMyToys(data);
+            })
+    }, [user])
+
     return (
         <div>
             <Navbar></Navbar>
-            
+            <table className="table w-full">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>
+                            <label>
+                                <input type="checkbox" className="checkbox" />
+                            </label>
+                        </th>
+                        <th className="text-center lg:pl-12">Seller Name</th>
+                        <th>Toy Name</th>
+                        <th>Sub-category</th>
+                        <th>Price</th>
+                        <th>Available Quantity</th>
+                        <th>View Details button</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        myToys.map(myToy => <MyToy
+                            key={myToy._id}
+                            myToy={myToy}
+                        ></MyToy>)
+                    }
+                </tbody>
+            </table>
+            <Footer></Footer>
         </div>
     );
 };

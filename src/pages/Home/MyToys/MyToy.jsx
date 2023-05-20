08e4b77e412037ef1a32ca612fe5f8f210ed467e
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-21 02:57:25 +0600

    DELETE Functionality Done

--- src/pages/Home/MyToys/MyToy.jsx
+++ src/pages/Home/MyToys/MyToy.jsx
@@ -1,5 +1,5 @@
 
-const MyToy = ({ myToy }) => {
+const MyToy = ({ myToy, handleDelete }) => {
     const { name, sellerName, subCategory, price, availableQuantity, _id, photoUrl } = myToy || {};
     return (
         <tr>
@@ -28,7 +28,7 @@ const MyToy = ({ myToy }) => {
             <td>{availableQuantity}</td>
             <th>
                 <button className="btn btn-info">Update</button>
-                <button className="btn btn-sm btn-circle">
+                <button onClick={() =>handleDelete(_id)} className="btn btn-sm btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
             </th>
--- src/pages/Home/MyToys/MyToys.jsx
+++ src/pages/Home/MyToys/MyToys.jsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useState } from "react";
+import Swal from "sweetalert2";
 import { AuthContext } from "../../../providers/AuthProvider";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
@@ -7,7 +8,8 @@ import MyToy from "./MyToy";
 const MyToys = () => {
 
     const { user } = useContext(AuthContext);
-    const [myToys, setMyToys] = useState([])
+    const [myToys, setMyToys] = useState([]);
+    // const [toys, setToys] = useState([]);
 
     useEffect(() => {
         fetch(`https://toy-marketplace-server-red.vercel.app/myToys/${user?.email}`)
@@ -16,6 +18,39 @@ const MyToys = () => {
                 setMyToys(data);
             })
     }, [user])
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log('Delete Confirmed');
+                fetch(`http://localhost:5000/myToys/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deleteCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Coffee has been deleted.',
+                                'success'
+                            )
+                            const remaining = myToys.filter(myT => myT._id !== _id);
+                            setMyToys(remaining);
+                        }
+                    })
+            }
+        })
+    }
 
     return (
         <div>
@@ -43,6 +78,7 @@ const MyToys = () => {
                         myToys.map(myToy => <MyToy
                             key={myToy._id}
                             myToy={myToy}
+                            handleDelete={handleDelete}
                         ></MyToy>)
                     }
                 </tbody>
